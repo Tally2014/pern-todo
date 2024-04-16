154a9feded6a9f98524a5ff92ce0afa4ff1194b6
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-16 22:30:48 +0200

    logic - impliment Create/Post endpoint operation to the postgresSQL database

--- server/index.js
+++ server/index.js
@@ -10,6 +10,19 @@ app.use(express.json());
 //ROUTES//
 
 //create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 
 //get all todos
 
