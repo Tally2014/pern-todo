f77af9d92a81ae7bf22a81caaa30db1543bc6c37
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-16 20:47:02 +0200

    logic - use cors middleware and express.json middleware to have access to json data in the app

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 app.listen(5100, () => {
   console.log("Sever started on Port: 5100");
