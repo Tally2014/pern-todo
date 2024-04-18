e2761ca2389b7f14550e6b62dd7ca9244b21a57e
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-19 01:44:00 +0200

    logic - Delete a selected todo from the server/postgres database

--- client/src/components/ListTodo.jsx
+++ client/src/components/ListTodo.jsx
@@ -13,6 +13,18 @@ export default function ListTodo() {
     }
   };
 
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5100/todos/${id}`, {
+        method: "DELETE",
+      });
+
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   useEffect(() => {
     getTodos();
   }, []);
@@ -49,6 +61,7 @@ export default function ListTodo() {
                 <a
                   href="#"
                   class="font-medium text-red-600 dark:text-red-500 hover:underline"
+                  onClick={() => deleteTodo(todo.todo_id)}
                 >
                   Delete
                 </a>
