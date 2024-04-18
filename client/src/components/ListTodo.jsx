8648c5ce4260b7d62a1e127d5da60cccc261ea30
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-19 01:29:56 +0200

    logic - map all todos data to the list Table

--- client/src/components/ListTodo.jsx
+++ client/src/components/ListTodo.jsx
@@ -35,30 +35,32 @@ export default function ListTodo() {
           </tr>
         </thead>
         <tbody>
-          <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Magic Mouse 2
-            </th>
-            <td class="px-6 py-4 text-right">
-              <a
-                href="#"
-                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          {todos.map((todo) => (
+            <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+              <th
+                scope="row"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
               >
-                Edit
-              </a>
-            </td>
-            <td class="px-6 py-4 text-right">
-              <a
-                href="#"
-                class="font-medium text-red-600 dark:text-red-500 hover:underline"
-              >
-                Delete
-              </a>
-            </td>
-          </tr>
+                {todo.description}
+              </th>
+              <td class="px-6 py-4 text-right">
+                <a
+                  href="#"
+                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Edit
+                </a>
+              </td>
+              <td class="px-6 py-4 text-right">
+                <a
+                  href="#"
+                  class="font-medium text-red-600 dark:text-red-500 hover:underline"
+                >
+                  Delete
+                </a>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
