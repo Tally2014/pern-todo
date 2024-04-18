ad1fdb3dba4480cc676db1513398f3c1746b46f2
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-19 00:26:52 +0200

    style - InputTodo JSX Markup and stailwind styling

--- client/src/components/InputTodo.jsx
+++ client/src/components/InputTodo.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function InputTodo() {
-  return <div>Input Todo</div>;
+  return (
+    <>
+      <h1 className="text-3xl text-center mt-5">Pern Todo List</h1>
+      <form className="flex justify-center mt-5">
+        <input
+          type="text"
+          class=" w-[50%] rounded-md border-2 mx-1 border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+        />
+
+        <button
+          class="inline-block shrink-0 rounded-md 
+        border border-green-600 bg-green-600 
+        px-12 py-3 text-sm font-medium text-white transition 
+        hover:bg-transparent hover:text-green-600 focus:outline-none 
+        focus:ring active:text-green-500"
+        >
+          Add
+        </button>
+      </form>
+    </>
+  );
 }
