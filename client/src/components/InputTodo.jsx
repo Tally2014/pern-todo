9bb03e957a9a4311ff31c2bbf313d3cf6db0ff38
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-04-19 00:49:18 +0200

    logic - POST todo to the server and postgres

--- client/src/components/InputTodo.jsx
+++ client/src/components/InputTodo.jsx
@@ -2,15 +2,29 @@ import { useState } from "react";
 
 export default function InputTodo() {
   const [description, setDescription] = useState("");
-  console.log(description);
+
+  const onSbumitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = await fetch("http://localhost:5100/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <>
       <h1 className="text-3xl text-center mt-5">Pern Todo List</h1>
-      <form className="flex justify-center mt-5">
+      <form className="flex justify-center mt-5" onSubmit={onSbumitForm}>
         <input
           type="text"
-          class=" w-[50%] rounded-md border-2 mx-1 border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+          class=" w-[50%] rounded-md border-2 mx-1 border-gray-200 bg-white text-sm text-gray-700 shadow-sm focus:outline-green-200"
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />
@@ -18,7 +32,7 @@ export default function InputTodo() {
         <button
           class="inline-block shrink-0 rounded-md 
         border border-green-600 bg-green-600 
-        px-12 py-3 text-sm font-medium text-white transition 
+        px-5 py-3 text-sm font-medium text-white transition 
         hover:bg-transparent hover:text-green-600 focus:outline-none 
         focus:ring active:text-green-500"
         >
